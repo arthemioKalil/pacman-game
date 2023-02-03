a43d6b45b5e72a4fa8ef4f7dcad9a8d0ea513950
artenks <artenkes@gmail.com>
2023-02-03 14:21:58 -0300

    implementando inteligencia ao fantasma

--- Assets/Scripts/Ghosts/GhostAI.cs
+++ Assets/Scripts/Ghosts/GhostAI.cs
@@ -6,16 +6,38 @@ public class GhostAI : MonoBehaviour
     private GhostMove _ghostMove;
 
     private Transform _pacman;
-    void Start()
+
+    public void StartMoving()
+    {
+        _ghostMove.CharacterMotor.enabled = true;
+    }
+    public void StopMoving()
+    {
+        _ghostMove.CharacterMotor.enabled = false;
+    }
+    public void Reset()
+    {
+        _ghostMove.CharacterMotor.ResetPosition();
+    }
+
+    void Awake()
     {
         _ghostMove = GetComponent<GhostMove>();
-        _ghostMove.OnUpdateMoveTarget += _ghostMove_OnUpdateMoveTarget;
-
         _pacman = GameObject.FindWithTag("Player").transform;
+
+        _ghostMove.OnUpdateMoveTarget += _ghostMove_OnUpdateMoveTarget;
     }
 
     private void _ghostMove_OnUpdateMoveTarget()
     {
         _ghostMove.SetTargetMoveLocation(_pacman.position);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.GetComponent<Life>().RemoveLife();
+        }
     }
 }
